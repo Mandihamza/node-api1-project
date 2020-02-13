4beb0ae73bf8e681fbd6962a5245b22d3368a635
mandihamza <mandihamza@gmail.com>
2020-02-12 20:34:24 -0700

    Adds post request for new user

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ server.get('/api/users', ( req, res ) => {
     });
   });
 
+
+// Get users by id
   server.get('/api/users/:id', ( req, res ) => {
     Users.findById(req.params.id)
     .then(user => {
         if (user) {
-            res.status(200).json(user);
+            res
+            .status(200)
+            .json(user);
         } else {
             res
             .status(404)
@@ -37,12 +41,38 @@ server.get('/api/users', ( req, res ) => {
         }
     })
     .catch(() => {
-        res.status(500)
+        res
+        .status(500)
         .json({ errorMessage: 'The user information could not be retrieved.' })
     });
   });
+
+  //Post new user
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    Users.insert(userInfo)
+    .then(users => {
+        res
+        .status(201)
+        .json(users)
+    })
+    .catch(() => {
+        res
+        .status(500)
+        .json({ errorMessage: 'There was an error while saving the user to the database' })
+    });
+});
+
 const port = 8080
 
 server.listen(port, () => {
     console.log(`server started at http://localhost:${port}`)
+})
+
+server.post('/api/users', (req, res) => {
+    Users.insert(req.body)
+    const newUser = {
+
+    }
 })
