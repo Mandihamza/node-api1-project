651760beceb1ba45868124aa40ecd35c7570beec
mandihamza <mandihamza@gmail.com>
2020-02-12 21:53:24 -0700

    Adds put endpoint

--- index.js
+++ index.js
@@ -83,6 +83,34 @@ server.delete('/api/users/:id', (req, res) => {
     });
 })
 
+//Put user
+server.put('/api/users/:id', (req, res) => {
+    const {name, bio} = req.body;
+
+    if (!name || !bio) {
+        res
+        .status(400)
+        .json({ errorMessage: 'Please provide name and bio for the user.'})
+    } else {
+        Users.update(req.params.id, req.body)
+        .then(user => {
+            if (user) {
+                res
+                .status(200).json(user);
+            } else {
+                res
+                .status(404)
+                .json({ message: 'The user with the specified ID does not exist.' })
+            }
+        })
+        .catch(() => {
+            res
+            .status(500)
+            .json({ errorMessage: 'The user information could not be modified.' })
+        })
+    }
+})
+
 const port = 8080
 
 server.listen(port, () => {
