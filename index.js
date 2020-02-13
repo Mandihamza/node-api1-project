76c9183a36563b968051ec824762051aec8cfa12
mandihamza <mandihamza@gmail.com>
2020-02-12 17:42:59 -0700

    Server runs on port 8080

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get("/", (req, res) => {
+	res.json({ message: "Server is running 👍" })
+})
+
+const port = 8080
+
+server.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`)
+})
