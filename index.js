27247824078950b561cdcda64d1bee99f77b705b
mandihamza <mandihamza@gmail.com>
2020-02-12 19:39:00 -0700

    Adds GET request for users by id

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const express = require('express')
+const express = require('express');
 
-const server = express()
+const Users = require('./data/db.js');
 
-server.use(express.json())
+const server = express();
+
+server.use(express.json());
 
 server.get("/", (req, res) => {
-	res.json({ message: "Server is running 👍" })
-})
+    res.json({ message: "Server is running 👍" })
+});
 
+// Get array of all users
+server.get('/api/users', ( req, res ) => {
+    Users.find()
+    .then(users => {
+        res
+        .status(200)
+        .json(users);
+    })
+    .catch(() => {
+        res
+        .status(500)
+        .json({errorMessage: 'Please provide name and bio for the user.'});
+    });
+  });
+
+  server.get('/api/users/:id', ( req, res ) => {
+    Users.findById(req.params.id)
+    .then(user => {
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res
+            .status(404)
+            .json({ message: 'The user with the specified ID does not exist.' })
+        }
+    })
+    .catch(() => {
+        res.status(500)
+        .json({ errorMessage: 'The user information could not be retrieved.' })
+    });
+  });
 const port = 8080
 
 server.listen(port, () => {
